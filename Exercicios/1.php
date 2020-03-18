3823dbfae615e27dca5ee77824ca596fd532a5f7
Eduardo Machado Rezende <52138771+eduardomachadorezende@users.noreply.github.com>
2020-03-17 21:36:22 -0300

    Add files via upload

---
+++ Exercicios/1.php
@@ -0,0 +1,13 @@
+<?php
+    $numero = 10;
+
+    if($numero < 0){
+        echo "Negativo\n";
+    }else if($numero > 0){
+        echo "Positivo\n";
+    }else{
+        echo "Igual a Zero\n";
+    }
+
+
+?>
---
+++ Exercicios/2.php
@@ -0,0 +1,9 @@
+<?php
+    $numero = 50;
+
+    for($i = 0; $i <= 100000000000; $i++ ){
+        echo "{$numero}x{$i} = ". $numero * $i."\n";
+    }
+
+
+?>
